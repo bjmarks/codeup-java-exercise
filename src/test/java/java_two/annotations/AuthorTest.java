6e49fdad09d03cb81970f6182b92dabbc1f0217c
Benjamin Marks <bjmarks13@yahoo.com>
2022-09-06 11:55:47 -0500

    commit

--- src/test/java/java_two/annotations/AuthorTest.java
+++ src/test/java/java_two/annotations/AuthorTest.java
@@ -1,22 +1,20 @@
 package java_two.annotations;
 
 public class AuthorTest {
+        @SuppressWarnings({"deprecation", "unchecked"})
     public static void main(String[] args) {
-//    Author author = new Author("Sandi Metz");
-//        author.addBook("Practical Object-Oriented Design in Ruby");
-//        author.addBook("99 Bottles of OOP");
-//        for (String book: author.getBooks()) {
-//        System.out.println(book);
-//    }
+    Author author = new Author("Sandi Metz");
+        author.addBook("Practical Object-Oriented Design in Ruby");
+        author.addBook("99 Bottles of OOP");
+        for (String book: author.getBooks()) {
+        System.out.println(book);
     }
-//    @SuppressWarnings({"deprecation", "unchecked"})
-    public static void fix2() {
-//        Author author = new Author("Sandi", "Metz");
-//        author.addBook("Practical Object-Oriented Design in Ruby");
-//        author.addBook("99 Bottles of OOP");
-//        for (String book: author.publishedBooks()) {
-//            System.out.println(book);
-//        }
+        Author author2 = new Author("Sandi Metz");
+        author2.addBook("Practical Object-Oriented Design in Ruby");
+        author2.addBook("99 Bottles of OOP");
+        for (String book: author2.publishedBooks()) {
+            System.out.println(book);
+        }
     }
 }
 
